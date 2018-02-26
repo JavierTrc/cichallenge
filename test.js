52ebbf2f3bb897870eacbc9868d48d863ebe11e9
Javier Troconis <jtronconis@endava.com>
2018-02-26 15:14:34 -0500

    Changed problems with the tests

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
-var test= require('test');
 var assert = require('assert')
 
 function test() {
     assert.equal(2 + 2, 4);
+    assert.equal(2 + 3, 5);
 }
 
 if (module == require.main) require('test').run(test);
