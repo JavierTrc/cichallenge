1c028c8a627bfb610d957612037d2aea416c35ff
Javier Troconis <jav.troc28@gmail.com>
2018-03-12 09:40:40 -0500

    Still testing jenkins..

--- test.js
+++ test.js
@@ -4,6 +4,7 @@ function test() {
     assert.equal(2 + 1, 3);
     assert.equal(2 + 2, 4);
     assert.equal(2 + 3, 5);
+    assert.equal(4 + 2, 6);
 }
 
 if (module == require.main) require('test').run(test);
